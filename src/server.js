7793fccb3002f5fd13c56847e5ff2f59a20c0d5a
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-02-27 15:16:58 -0300

    Exemplo de POST

--- src/server.js
+++ src/server.js
@@ -2,14 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-  response.send(`Olá ${user}. O ID informado foi: ${id}`);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-  response.send(`Você está na página ${page} de ${limit}`);
+app.post("/users", (request, response) => {
+  response.send(`Exemplo de POST`);
 });
 
 const PORT = 3333;
