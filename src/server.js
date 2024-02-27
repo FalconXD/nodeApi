9bb317d43412659b0ecf5e19e3b1631277796964
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-02-27 14:50:18 -0300

    Utilização de parêmetros GET

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id", (request, response) => {
+  response.send(`Olá ${request.params.id}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
