76103a5a17ac2a69248e047fa70f2aa0821d67e8
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-02-27 22:13:38 -0300

    Exemplos de middleware e de como aplica-lo nas rotas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,24 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+// -------------- Exemplo de Middleware -----------------
+// function myMiddleware(request, response, next){
+//   console.log("Middleware acionado!");
+
+//   if(!request.body.isAdmin){
+//     return response.json({mesage: "User unauthorized"});
+//   }
+
+//   next();
+// }
+
 const usersController = new UsersController();
+
+// Aplicando o Middleware para todas as rotas
+//usersRoutes.use(myMiddleware);
+
+// Middleware aplicado para rota específica
+// usersRoutes.post("/", myMiddleware, usersController.create);
 
 usersRoutes.post("/", usersController.create);
 
