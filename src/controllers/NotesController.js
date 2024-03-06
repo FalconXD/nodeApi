348c64a010cad493b36e9052197fd7c93e9965ca
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-03-06 16:05:13 -0300

    Inclusão de listagem de tags no retorno das notas

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -57,10 +57,40 @@ class NotesController{
   }
 
   async index(request, response){
-    const {user_id, title} = request.query;
-    const notes = await knex("notes").where({user_id}).whereLike("title",`%${title}%`).orderBy("title");
+    const {user_id, title, tags} = request.query;
 
-    return response.json(notes);
+    let notes;
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag => tag.trim());
+      
+      notes = await knex("tags")
+        .select([
+          "notes.id",
+          "notes.title",
+          "notes.user_id",
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
+
+    }else{
+      notes = await knex("notes").where({user_id}).whereLike("title",`%${title}%`).orderBy("title");
+    }
+
+    const userTags = await knex("tags").where({user_id});
+    const notesWhitTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+    return response.json(notesWhitTags);
   }
 }
 
